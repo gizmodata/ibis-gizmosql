f6441f46ac2e1196ebbfd36ca440f78a04631851
Philip Moore <prmoore77@hotmail.com>
2026-01-09 16:40:14 -0500

    bump version 0.0.12 -> 0.0.13

--- ibis_gizmosql/__init__.py
+++ ibis_gizmosql/__init__.py
@@ -34,7 +34,7 @@ from packaging.version import parse as vparse
 
 from ibis_gizmosql.converter import DuckDBPandasData
 
-__version__ = "0.0.12"
+__version__ = "0.0.13"
 
 if TYPE_CHECKING:
     from collections.abc import Iterable, Mapping, MutableMapping, Sequence
