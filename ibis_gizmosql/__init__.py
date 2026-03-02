8609641d16cb84c6c00e306fbb4e28ce01668d0a
Philip Moore <prmoore77@hotmail.com>
2026-03-02 12:32:30 -0500

    bump version 0.2.1 -> 1.0.0

--- ibis_gizmosql/__init__.py
+++ ibis_gizmosql/__init__.py
@@ -35,7 +35,7 @@ from packaging.version import parse as vparse
 
 from ibis_gizmosql.converter import DuckDBPandasData
 
-__version__ = "0.2.1"
+__version__ = "1.0.0"
 
 # Default batch size for ADBC bulk ingest operations
 _INGEST_BATCH_SIZE = 10_000
